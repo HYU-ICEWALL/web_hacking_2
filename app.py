60a517a7d8c4c2a8284751a639fb292ec41f5f02
IronD414 <kcy1760@gmail.com>
2023-09-26 19:39:29 +0900

    230926

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import os
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, session
 from models import db, User
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Icewall!'
+    if 'username' not in session:
+        return redirect('/login/')
+    else:
+        username = session['username']
+        return "Hello, " + username # 로그인 상태라면 username 출력
     
 @app.route('/signup/', methods=['GET', 'POST'])
 def signup():
@@ -22,15 +26,39 @@ def signup():
 
             db.session.add(usertable)
             db.session.commit()
-            return "회원가입 성공"
+            return redirect('/')
 
     else:
         return render_template("signup.html")
 
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        # print(username) -> 터미널창에서 username 확인 가능, 디버깅 시 사용
+
+        if not(username and password):
+            return "입력되지 않은 정보가 있습니다"
+        else:
+            user = User.query.filter_by(username=username).first()
+            if user:
+                if user.password == password:
+                    session['username'] = username
+                    return redirect('/')
+                else:
+                    return "비밀번호가 다릅니다"
+            else:
+                return "사용자가 존재하지 않습니다"
+    else:
+        return render_template('login.html')
 if __name__ == "__main__":
     with app.app_context():
         basedir = os.path.abspath(os.path.dirname(__file__)) # 현재 파일이 있는 폴더 경로
         dbfile = os.path.join(basedir, 'db.sqlite') # 데이터베이스 파일 생성
+
+        app.config['SECRET_KEY'] = "ICEWALL" # 세션관리 및 암호화를 위한 시크릿키 생성
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
         app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -8,3 +9,11 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True) # 자동으로 증가하는 User 모델의 기본 키
     username = db.Column(db.String(100), unique=True, nullable=False) # 같은 값 저장 X, 빈 값 X
     password = db.Column(db.String(20), nullable=False) # 빈 값 X
+
+class Post(db.Model):
+    __tablename__ = 'post'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    content = db.Column(db.String(1000))
+    datetime = db.Column(db.DateTime, default=datetime.now(), onupdate=datetime.now())
